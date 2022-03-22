7ba601ce405243b4a3d4b325e1c2cdaf5d96c438
utukJ <utukphd@gmail.com>
2022-03-22 22:33:24 +0100

    first attempt at task 9

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * _strcpy - prints second half of string followed by new line
+ *
+ * @dest: destination buffer
+ * @src: string to be copied
+ *
+ * Return: buffer pointed to by dest(char *) 
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	i = 0;
+
+	while (src[i] != '\0')
+	{
+		*(dest + i) = src[i];
+		i++;
+	}
+	*(dest + i) = '\0';
+	return (dest);
+}
