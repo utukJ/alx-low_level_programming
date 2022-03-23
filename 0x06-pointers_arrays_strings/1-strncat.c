fe315b803abc8ad83940a36026bf26c5f6c42324
utukJ <utukphd@gmail.com>
2022-03-23 21:19:16 +0100

    completed task 1

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,23 @@
+/**
+ * _strncat - concatenates two strings (only at most n bytes of second string)
+ *
+ * @dest: destination string
+ * @src: other string to be concatenated (at most n bytes)
+ * @n: max number of bytes of src to concatenate
+ *
+ * Return: pointer to dest
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int i, j;
+
+	for (i = 0; dest[i] != '\0'; i++)
+		;
+	
+	for (j = 0; src[j] != '\0' && j < n; j++)
+		*(dest + i + j) = src[j];
+	
+	*(dest + i + j) = '\0';
+	
+	return (dest);
+}
