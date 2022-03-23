2bd3d543234debe782755e796986481a955ad2b8
utukJ <utukphd@gmail.com>
2022-03-23 21:27:16 +0100

    completed task 2

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,22 @@
+/**
+ * _strncpy - copies a string
+ *
+ * @dest: pointer to destination
+ * @src: pointer to source
+ * @n: max number of bytes to copy
+ *
+ * Return: pointer to dest
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	for (i = 0; src[i] != '\0' && i < n; i++)
+	{
+		dest[i] = src[i];
+	}
+
+	dest[i] = '\0';
+
+	return (dest);
+}
