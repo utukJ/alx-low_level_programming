de4aadc79b2d5dd32e12d4622830959bc0112e41
utukJ <utukphd@gmail.com>
2022-03-24 08:16:25 +0100

    completed task 2

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -16,7 +16,11 @@ char *_strncpy(char *dest, char *src, int n)
 		dest[i] = src[i];
 	}
 
-	dest[i] = src[i];
+	if (i < n)
+	{
+		for (; dest[i] != '\0'; i++)
+			dest[i] = '\0';
+	}
 
 	return (dest);
 }
