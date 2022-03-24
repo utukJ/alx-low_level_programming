db639e68c44090c8986a5d6d81dc54e1165200ab
utukJ <utukphd@gmail.com>
2022-03-24 07:56:51 +0100

    fixed trailing whitespace

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -13,11 +13,11 @@ char *_strncat(char *dest, char *src, int n)
 
 	for (i = 0; dest[i] != '\0'; i++)
 		;
-	
+
 	for (j = 0; src[j] != '\0' && j < n; j++)
 		*(dest + i + j) = src[j];
-	
+
 	*(dest + i + j) = '\0';
-	
+
 	return (dest);
 }
