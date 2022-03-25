5e1668a6c2854da0751de7f59fc4d0a39f049f5c
utukJ <utukphd@gmail.com>
2022-03-25 11:58:27 +0100

    added header file

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,1 +1,5 @@
 char *_strcat(char *dest, char *src);
+char *_strncat(char *dest, char *src, int n);
+char *_strncpy(char *dest, char *src, int n);
+int _strcmp(char *str1, char *str2);
+void reverse_array(int *a, int n);
