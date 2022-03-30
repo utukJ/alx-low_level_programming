326fd1ec30ee7106c5797904d37e54cc34d1e44c
utukJ <utukphd@gmail.com>
2022-03-30 22:27:06 +0100

    fixed file name

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * _puts_recursion - prints a string using recursion
+ * @s: string to be printed
+ *
+ * Return: nothing
+ */
+void _puts_recursion(char *s)
+{
+	putchar(*s);
+
+	if (*(s+1) != 0)
+	{
+		_puts_recursion(s+1);
+	}
+}
