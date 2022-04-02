73e3feb938b923eac2f67d6f62d26fffd401d6e1
utukJ <utukphd@gmail.com>
2022-04-02 05:08:16 +0100

    completed prject 9

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -1,8 +1,5 @@
-#ifndef FILE_HOLBERTON
-#define FILE_HOLBERTON
-
-/*copy list from task*/
-
+#ifndef MAIN_FILE
+#define MAIN_FILE
 int _putchar(char c);
 int _islower(int c);
 int _isalpha(int c);
@@ -23,5 +20,4 @@ char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
-
 #endif
