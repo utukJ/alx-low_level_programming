36a950d1153406f6007f703608ec3a2640253a35
utukJ <utukphd@gmail.com>
2022-04-05 11:38:28 +0100

    completed task 2

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,36 @@
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * str_concat - concatenates two strings
+ * @s1: first string
+ * @s2: second string
+ *
+ * Return: pointer to concatenated string
+ */
+char *str_concat(char *s1, char *s2)
+{
+	int size; 
+	int i, j;
+	char *concat;
+
+	if (s1 == NULL)
+		return (str_concat("", s2));
+	if (s2 == NULL)
+		return (str_concat(s1, ""));
+
+	size = strlen(s1) + strlen(s2) + 1;
+	concat = malloc(size);
+
+	if (concat == NULL)
+		return (NULL);
+
+	for (i = 0; i < strlen(s1); i++)	
+		concat[i] = s1[i];
+
+	for (j = 0; j < strlen(s2); j++)
+		concat[i + j] = s2[j];
+
+	concat[i+j] = '\0';
+	return (concat);
+}
