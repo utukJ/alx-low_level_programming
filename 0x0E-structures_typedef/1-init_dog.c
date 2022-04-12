6464e0a39f294178024e85fab27b8dc057b53bec
utukJ <utukphd@gmail.com>
2022-04-12 01:33:51 +0100

    completed tasks 0 and 1

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,17 @@
+#include "dog.h"
+
+/**
+ * init_dog - initialize struct dog
+ * @d: pointer to initialized struct
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: dog's owner
+ * 
+ * Return: initialized struct with fields
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+    d->name = name;
+    d->age = age;
+    d->owner = owner;
+}
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,14 @@
+/**
+ * struct dog - dog info
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
+ * 
+ * Description: Stores dog information
+ */
+struct dog
+{
+    char *name;
+    float age;
+    char *owner;
+};
