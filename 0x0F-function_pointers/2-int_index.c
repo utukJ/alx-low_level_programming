d78fcb97317a03e17ad6ca266b30a80dc63ccfde
utukJ <utukphd@gmail.com>
2022-04-16 09:47:27 +0100

    simplified3

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -1,5 +1,5 @@
 #include "function_pointers.h"
-
+#include <stdlib.h>
 
 /**
  * int_index - Print the index of an array
@@ -13,10 +13,7 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 
-	if (size <=  0)
-		return (-1);
-
-	if (array == NULL || cmp == NULL)
+	if (size <=  0 || array == NULL || cmp == NULL)
 		return (-1);
 
 	for (i = 0; i < size; i++)
