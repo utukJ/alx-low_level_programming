b0c1a7e192dd079e40495e130c699d1b53991642
utukJ <utukphd@gmail.com>
2022-04-25 12:11:00 +0100

    completed task 1

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * listint_len - prints all the elements of a listint_t list
+ * @listint_t: listint_t list
+ *
+ * Return: number of nodes
+ */
+size_t listint_len(const listint_t *h)
+{
+	size_t count;
+	const listint_t *node;
+	
+	count = 0;
+
+	for (node = h; node != NULL; node = node->next)
+		count++;
+
+	return (count);
+}
