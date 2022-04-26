68236afb3467a2357b583f70c0fdcc457169410a
utukJ <utukphd@gmail.com>
2022-04-26 06:38:02 +0100

    completed task 2

---
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+
+/**
+ * add_nodeint - adds a new node at the beginning of a linked list
+ * @head: pointer to head of list
+ * @n: new node int value
+ *
+ * Return: address of the new element or NULL
+ */
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+	listint_t *new_head;
+
+	new_head->n = n;
+	if (head != NULL)
+		new_head->next = *head;
+	else
+		new_head->next = NULL;
+
+	head = &new_head;
+
+	return (*head);
+}
