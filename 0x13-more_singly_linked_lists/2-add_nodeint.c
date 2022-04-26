85679d749e20ae8d5adc82311dc94a831b8be7da
utukJ <utukphd@gmail.com>
2022-04-26 06:46:05 +0100

    fixed bugs

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -10,7 +10,10 @@
  */
 listint_t *add_nodeint(listint_t **head, const int n)
 {
-	listint_t *new_head;
+	listint_t *new_head = malloc(sizeof(listint_t));
+
+	if (new_head == NULL)
+		return (NULL);
 
 	new_head->n = n;
 	if (head != NULL)
